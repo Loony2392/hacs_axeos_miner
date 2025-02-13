f579a8c606355b63bcd95adad558b5b5fd92b414
Loony <77867402+Loony2392@users.noreply.github.com>
2025-02-14 00:46:37 +0100

    fix: Import DOMAIN constant in Axeos Miner config flow

--- custom_components/axeos_miner/config_flow.py
+++ custom_components/axeos_miner/config_flow.py
@@ -1,7 +1,7 @@
 from homeassistant import config_entries
 import voluptuous as vol
-from .const import DOMAIN
 import aiohttp
+from .const import DOMAIN
 
 class AxeosMinerConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Axeos Miner."""
